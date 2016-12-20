905230d15e8ba1f4a62f2a1cfa0539ac06f5a5df
Ben Gottlieb <ben@standalone.com>
2016-12-19 21:11:28 -0600

    finished cancel button, added URL following

--- WebViewController/WebViewController.swift
+++ WebViewController/WebViewController.swift
@@ -48,6 +48,8 @@ class WebViewController: UIViewController {
 
 			self.titleBar.setup()
 			self.webView.scrollView.delegate = self.titleBar
+			self.webView.navigationDelegate = self
+			self.titleBar.webViewController = self
 		}
 	}
 	
@@ -58,53 +60,118 @@ class WebViewController: UIViewController {
 			self.titleBar.set(url: url)
 		}
 	}
+	
+	func didEnterURL(_ url: URL?) {
+		guard let url = url else { return }
+		self.url = url
+		self.webView.load(URLRequest(url: url))
+	}
+}
+
+extension WebViewController: WKNavigationDelegate {
+	public func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+		if (error as NSError).domain == "NSURLErrorDomain", (error as NSError).code == -1003 {		//couldn't find host, switch to google
+			guard let current = self.url, let url = URL(string: "https://www.google.com/#q=\(current.absoluteString)") else { return }
+			self.webView.load(URLRequest(url: url))
+			return
+		}
+		print("Provisional load failed: \(error)")
+	}
+	
+	public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+		print("Navigation failed: \(error)")
+	}
+	
+	public func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+		
+		completionHandler(.useCredential, challenge.proposedCredential)
+	}
 }
 
 extension WebViewController {
 	
-	class TitleBar: UIView, UIScrollViewDelegate {
+	class TitleBar: UIView, UIScrollViewDelegate, UITextFieldDelegate {
 		static let maxHeight: CGFloat = 64
 		static let minHeight: CGFloat = 40
 		
+		weak var webViewController: WebViewController!
+		var originalText: String = ""
 		var fieldBackgroundMargin: CGFloat = 10
 		var fieldBackgroundMaxMargin: CGFloat = 10
 		var fieldBackgroundHeight: CGFloat = 27
 		var fieldBackground: UIView!
+		var cancelButtonRight: CGFloat = 60.0
 		let fieldBackgroundTopMargin: CGFloat = 20
 		var effectiveScrollTop: CGFloat!
 		var scrollView: UIScrollView?
+		var editing = false
 		var cancelButton: UIButton!
-		var cancelButtonWidthConstraint: NSLayoutConstraint!
+		var cancelButtonRightConstraint: NSLayoutConstraint!
+		var isCancelButtonVisible = false { didSet {
+			self.cancelButtonRightConstraint?.constant = self.isCancelButtonVisible ? -self.fieldBackgroundMargin : self.cancelButtonRight
+		}}
 		
 		var urlField: UITextField!
 		var urlFieldFont: UIFont {
-			return UIFont.systemFont(ofSize: self.fieldBackground.bounds.height * 0.6)
+			return UIFont.systemFont(ofSize: 12 + self.displayedHeightFraction * 4)
 		}
 		
-		func setEditable() {
-			guard let scrollView = self.scrollView, self.displayedHeightFraction != 1.0 else { return }
+		func tapped() {
+			guard let scrollView = self.scrollView else { return }
 			
-			let delta = (TitleBar.maxHeight - TitleBar.minHeight)
-			self.effectiveScrollTop = scrollView.contentOffset.y - delta
+			if self.displayedHeightFraction != 1.0 {
+				let delta = (TitleBar.maxHeight - TitleBar.minHeight)
+				self.effectiveScrollTop = scrollView.contentOffset.y - delta
 			
-			scrollView.setContentOffset(CGPoint(x: scrollView.contentOffset.x, y: self.effectiveScrollTop), animated: true)
-			DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { self.makeFieldEditable() }
+				scrollView.setContentOffset(CGPoint(x: scrollView.contentOffset.x, y: self.effectiveScrollTop), animated: true)
+			} else {
+				DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { self.makeFieldEditable(true) }
+			}
 		}
 		
-		func makeFieldEditable() {
+		func makeFieldEditable(_ editable: Bool) {
 			let str = NSAttributedString(string: self.urlField.text ?? "", attributes: [NSFontAttributeName: self.urlField.font!])
 			let size = str.size()
 			let offset = ((self.urlField.bounds.width - size.width) / 2) - 2
-			
-			UIView.animate(withDuration: 0.2, animations: {
-				self.urlField.transform = CGAffineTransform(translationX: -offset, y: 0)
-			}) { complete in
-				self.urlField.isUserInteractionEnabled = true
-				self.urlField.transform = CGAffineTransform.identity
-				self.urlField.textAlignment = .left
-				self.urlField.selectAll(nil)
-				self.urlField.clearButtonMode = .whileEditing
+			let duration: TimeInterval = 0.2
+
+			if editable {
+				if self.editing { return }
+				self.editing = true
+				self.isCancelButtonVisible = true
+				self.originalText = self.urlField.text ?? ""
+				UIView.animate(withDuration: duration, animations: {
+					self.updateConstraintsIfNeeded()
+					self.urlField.transform = CGAffineTransform(translationX: -(offset - self.cancelButtonRight / 2), y: 0)
+				}) { complete in
+					self.urlField.isUserInteractionEnabled = true
+					self.cancelButton.isUserInteractionEnabled = true
+					self.urlField.transform = CGAffineTransform.identity
+					self.urlField.textAlignment = .left
+					self.urlField.selectAll(nil)
+					self.urlField.clearButtonMode = .whileEditing
+				}
+			} else {
+				if !self.editing { return }
+				self.isCancelButtonVisible = false
+				self.urlField.clearButtonMode = .never
+				self.urlField.isUserInteractionEnabled = false
+				self.cancelButton.isUserInteractionEnabled = false
+
+				UIView.animate(withDuration: duration, animations: {
+					self.updateConstraintsIfNeeded()
+					self.urlField.transform = CGAffineTransform(translationX: offset + self.cancelButtonRight / 2, y: 0)
+				}) { complete in
+					self.urlField.transform = CGAffineTransform.identity
+					self.urlField.textAlignment = .center
+					self.editing = false
+				}
 			}
+		}
+		
+		func cancelEditing() {
+			self.urlField.text = self.originalText
+			self.makeFieldEditable(false)
 		}
 		
 		var currentHeight = TitleBar.maxHeight
@@ -114,11 +181,11 @@ extension WebViewController {
 			let maxDelta = TitleBar.maxHeight - TitleBar.minHeight
 			self.currentHeight = TitleBar.minHeight + maxDelta * self.displayedHeightFraction
 			
-			self.fieldBackgroundMargin = self.fieldBackgroundMaxMargin * self.displayedHeightFraction
+		//	self.fieldBackgroundMargin = self.fieldBackgroundMaxMargin * self.displayedHeightFraction
 			self.fieldBackground.translatesAutoresizingMaskIntoConstraints = false
 			
-			self.backgroundTopConstraint.constant = (self.fieldBackgroundMargin + self.fieldBackgroundTopMargin)
-			self.backgroundBottomConstraint.constant = -self.fieldBackgroundMargin
+			//self.backgroundTopConstraint.constant = (self.fieldBackgroundMargin + self.fieldBackgroundTopMargin)
+			//self.backgroundBottomConstraint.constant = -self.fieldBackgroundMargin
 
 			self.fieldBackground.alpha = self.displayedHeightFraction * self.displayedHeightFraction
 			self.titleHeightConstraint.constant = self.currentHeight
@@ -154,12 +221,6 @@ extension WebViewController {
 			self.fieldBackground.translatesAutoresizingMaskIntoConstraints = false
 			self.backgroundTopConstraint = NSLayoutConstraint(item: self.fieldBackground, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: (self.fieldBackgroundMargin + self.fieldBackgroundTopMargin))
 			self.backgroundBottomConstraint = NSLayoutConstraint(item: self.fieldBackground, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: -self.fieldBackgroundMargin)
-			self.addConstraints([
-				NSLayoutConstraint(item: self.fieldBackground, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1.0, constant: 0),
-				self.backgroundTopConstraint,
-				self.backgroundBottomConstraint
-			])
-			
 			self.fieldBackground.layer.cornerRadius = 5
 			self.fieldBackground.layer.masksToBounds = true
 			
@@ -171,30 +232,43 @@ extension WebViewController {
 			self.urlField = UITextField(frame: self.fieldBackground.frame)
 			self.addSubview(self.urlField)
 			self.urlField.translatesAutoresizingMaskIntoConstraints = false
+			self.urlField.autocorrectionType = .no
+			self.urlField.autocapitalizationType = .none
+			self.urlField.spellCheckingType = .no
+			
 			self.addConstraints([
 				NSLayoutConstraint(item: self.urlField, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: backgroundHeight),
-//				NSLayoutConstraint(item: self.urlField, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: backgroundWidth - 10),
 				NSLayoutConstraint(item: self.urlField, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: self.fieldBackgroundMargin + 5),
-//				NSLayoutConstraint(item: self.urlField, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1.0, constant: 0),
 				NSLayoutConstraint(item: self.urlField, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1.0, constant: (self.fieldBackgroundTopMargin + contentHeight / 2) - (self.bounds.height / 2)),
 			])
 			self.urlField.textAlignment = .center
 			self.urlField.adjustsFontSizeToFitWidth = true
 			self.urlField.font = self.urlFieldFont
+			self.urlField.returnKeyType = .go
+			self.urlField.delegate = self
 			
 			self.cancelButton = UIButton(type: .system)
+			self.cancelButton.addTarget(self, action: #selector(cancelEditing), for: .touchUpInside)
 			self.cancelButton.setTitle(NSLocalizedString("Cancel", comment: "Cancel"), for: .normal)
 			self.addSubview(self.cancelButton)
 			self.cancelButton.translatesAutoresizingMaskIntoConstraints = false
-			self.cancelButtonWidthConstraint = NSLayoutConstraint(item: self.cancelButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 0.0)
-			self.cancelButtonWidthConstraint.isActive = false
+			self.cancelButtonRightConstraint = NSLayoutConstraint(item: self.cancelButton, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: self.isCancelButtonVisible ? -self.fieldBackgroundMargin : self.cancelButtonRight)
 			self.addConstraints([
 				NSLayoutConstraint(item: self.cancelButton, attribute: .left, relatedBy: .equal, toItem: self.urlField, attribute: .right, multiplier: 1.0, constant: 5),
-				NSLayoutConstraint(item: self.cancelButton, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: -10),
+				NSLayoutConstraint(item: self.cancelButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 60),
 				NSLayoutConstraint(item: self.cancelButton, attribute: .centerY, relatedBy: .equal, toItem: self.urlField, attribute: .centerY, multiplier: 1, constant: 0),
-				self.cancelButtonWidthConstraint,
+				self.cancelButtonRightConstraint,
 			])
-			self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(setEditable)))
+
+			self.addConstraints([
+				NSLayoutConstraint(item: self.fieldBackground, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: self.fieldBackgroundMargin),
+				NSLayoutConstraint(item: self.fieldBackground, attribute: .right, relatedBy: .equal, toItem: self.cancelButton, attribute: .left, multiplier: 1.0, constant: -self.fieldBackgroundMargin),
+				self.backgroundTopConstraint,
+				self.backgroundBottomConstraint
+			])
+			
+
+			self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
 		}
 		
 		func scrollViewDidScroll(_ scrollView: UIScrollView) {
@@ -206,8 +280,10 @@ extension WebViewController {
 			if scrollAmount < 0 {
 				self.displayedHeightFraction = 1.0
 			} else if scrollAmount < maxDelta {
+				self.makeFieldEditable(false)
 				self.displayedHeightFraction = 1.0 - scrollAmount / maxDelta
 			} else {
+				self.makeFieldEditable(false)
 				self.displayedHeightFraction = 0.0
 			}
 			scrollView.scrollIndicatorInsets = UIEdgeInsets(top: self.currentHeight, left: 0, bottom: 0, right: 0)
@@ -222,6 +298,22 @@ extension WebViewController {
 			bezier.addLine(to: CGPoint(x: bounds.width, y: bounds.height - lineWidth))
 			bezier.lineWidth = lineWidth
 			bezier.stroke()
+		}
+		
+		func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+			self.makeFieldEditable(false)
+			if let text = self.urlField.text, let url = URL(string: text) {
+				guard var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return false }
+				
+				if components.scheme == nil { components.scheme = "https" }
+				if components.host == nil {
+					components.host = components.path
+					components.path = ""
+				}
+				
+				self.webViewController?.didEnterURL(components.url)
+			}
+			return false
 		}
 	}
 }
