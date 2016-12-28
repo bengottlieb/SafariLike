2cb732ac325360b3e672d6555fe1ca11d7120a44
Ben Gottlieb <ben@standalone.com>
2016-12-28 06:04:05 -0600

    shrink both left and right toolbars when scrolling

--- WebViewController/SafarishIPadTitleBarView.swift
+++ WebViewController/SafarishIPadTitleBarView.swift
@@ -31,7 +31,6 @@ extension SafarishViewController {
 			self.leftToolbar.addConstraint(NSLayoutConstraint(item: self.leftToolbar, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: self.fieldBackgroundHMargin))
 			
 			self.addConstraints([
-				//NSLayoutConstraint(item: self.leftToolbar, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
 				NSLayoutConstraint(item: self.leftToolbar, attribute: .right, relatedBy: .equal, toItem: self.fieldBackground, attribute: .left, multiplier: 1.0, constant: 0),
 				NSLayoutConstraint(item: self.leftToolbar, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: self.fieldBackgroundTopMargin),
 				NSLayoutConstraint(item: self.leftToolbar, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0)
@@ -44,7 +43,6 @@ extension SafarishViewController {
 			self.rightToolbar.addConstraint(NSLayoutConstraint(item: self.rightToolbar, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: self.fieldBackgroundHMargin))
 			
 			self.addConstraints([
-				//NSLayoutConstraint(item: self.leftToolbar, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0),
 				NSLayoutConstraint(item: self.rightToolbar, attribute: .left, relatedBy: .equal, toItem: self.fieldBackground, attribute: .right, multiplier: 1.0, constant: 0),
 				NSLayoutConstraint(item: self.rightToolbar, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: self.fieldBackgroundTopMargin),
 				NSLayoutConstraint(item: self.rightToolbar, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0)
@@ -52,12 +50,14 @@ extension SafarishViewController {
 		}
 		
 		override var displayedHeightFraction: CGFloat { didSet {
+			let translation = (1.0 - self.displayedHeightFraction * self.displayedHeightFraction * self.displayedHeightFraction * self.displayedHeightFraction) * self.fieldBackgroundHMargin * 0.2
+
 			self.leftToolbar.alpha = self.displayedHeightFraction * self.displayedHeightFraction
-			self.leftToolbar.transform = CGAffineTransform(scaleX: self.displayedHeightFraction, y: self.displayedHeightFraction)
+			self.leftToolbar.transform = CGAffineTransform(translationX: translation, y: 0).scaledBy(x: self.displayedHeightFraction, y: self.displayedHeightFraction)
 
 			self.rightToolbar.alpha = self.displayedHeightFraction * self.displayedHeightFraction
-			self.rightToolbar.transform = CGAffineTransform(scaleX: self.displayedHeightFraction, y: self.displayedHeightFraction)
-			
+			self.rightToolbar.transform = CGAffineTransform(translationX: -translation, y: 0).scaledBy(x: self.displayedHeightFraction, y: self.displayedHeightFraction)
+		
 		}}
 		
 		
